9881dbacaee69374de05b7c08d9c69cbf4e56ad6
Johan Wänglöf <jwanglof@gmail.com>
2012-03-20 11:51:04 +0100

    Commented the dice-language

--- rdparse.rb
+++ rdparse.rb
@@ -223,79 +223,79 @@ class Parser
 
 end
 
-##############################################################################
-#
-# This part defines the dice language
-#
-##############################################################################
+# ##############################################################################
+# #
+# # This part defines the dice language
+# #
+# ##############################################################################
 
-class DiceRoller
+# class DiceRoller
         
-  def DiceRoller.roll(times, sides)
-    (1..times).inject(0) {|sum, _| sum + rand(sides) + 1 }
-  end
+#   def DiceRoller.roll(times, sides)
+#     (1..times).inject(0) {|sum, _| sum + rand(sides) + 1 }
+#   end
   
-  def initialize
-    @diceParser = Parser.new("dice roller") do
-      token(/\s+/)
-      token(/\d+/) {|m| m.to_i }
-      token(/./) {|m| m }
+#   def initialize
+#     @diceParser = Parser.new("dice roller") do
+#       token(/\s+/)
+#       token(/\d+/) {|m| m.to_i }
+#       token(/./) {|m| m }
       
-      start :expr do 
-        match(:expr, '+', :term) {|a, _, b| a + b }
-        match(:expr, '-', :term) {|a, _, b| a - b }
-        match(:term)
-      end
+#       start :expr do 
+#         match(:expr, '+', :term) {|a, _, b| a + b }
+#         match(:expr, '-', :term) {|a, _, b| a - b }
+#         match(:term)
+#       end
       
-      rule :term do 
-        match(:term, '*', :dice) {|a, _, b| a * b }
-        match(:term, '/', :dice) {|a, _, b| a / b }
-        match(:dice)
-      end
+#       rule :term do 
+#         match(:term, '*', :dice) {|a, _, b| a * b }
+#         match(:term, '/', :dice) {|a, _, b| a / b }
+#         match(:dice)
+#       end
 
-      rule :dice do
-        match(:atom, 'd', :sides) {|a, _, b| DiceRoller.roll(a, b) }
-        match('d', :sides) {|_, b| DiceRoller.roll(1, b) }
-        match(:atom)
-      end
+#       rule :dice do
+#         match(:atom, 'd', :sides) {|a, _, b| DiceRoller.roll(a, b) }
+#         match('d', :sides) {|_, b| DiceRoller.roll(1, b) }
+#         match(:atom)
+#       end
       
-      rule :sides do
-        match('%') { 100 }
-        match(:atom)
-      end
+#       rule :sides do
+#         match('%') { 100 }
+#         match(:atom)
+#       end
       
-      rule :atom do
-        # Match the result of evaluating an integer expression, which
-        # should be an Integer
-        match(Integer)
-        match('(', :expr, ')') {|_, a, _| a }
-      end
-    end
-  end
+#       rule :atom do
+#         # Match the result of evaluating an integer expression, which
+#         # should be an Integer
+#         match(Integer)
+#         match('(', :expr, ')') {|_, a, _| a }
+#       end
+#     end
+#   end
   
-  def done(str)
-    ["quit","exit","bye",""].include?(str.chomp)
-  end
+#   def done(str)
+#     ["quit","exit","bye",""].include?(str.chomp)
+#   end
   
-  def roll
-    print "[diceroller] "
-    str = gets
-    if done(str) then
-      puts "Bye."
-    else
-      puts "=> #{@diceParser.parse str}"
-      roll
-    end
-  end
+#   def roll
+#     print "[diceroller] "
+#     str = gets
+#     if done(str) then
+#       puts "Bye."
+#     else
+#       puts "=> #{@diceParser.parse str}"
+#       roll
+#     end
+#   end
 
-  def log(state = true)
-    if state
-      @diceParser.logger.level = Logger::DEBUG
-    else
-      @diceParser.logger.level = Logger::WARN
-    end
-  end
-end
+#   def log(state = true)
+#     if state
+#       @diceParser.logger.level = Logger::DEBUG
+#     else
+#       @diceParser.logger.level = Logger::WARN
+#     end
+#   end
+# end
 
 # Examples of use
 
